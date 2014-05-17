340130e18cb367e6830dd3abfab0cf3cec817d0f
Jamal <jamal_waites@yahoo.com>
2014-05-17 01:17:20 -0400

    first python scripts create

---
+++ simpleIFstatement.py
@@ -0,0 +1,31 @@
+'''
+a = 33
+if a >= 34:
+
+    print("biggest")
+
+elif a >= 21:
+
+    print("big")
+
+else:
+
+    print("not big")
+'''
+
+
+
+a = "fjog"
+if a >= "frog":
+
+    print("kermit")
+
+elif a >= "pig":
+
+    print("Mrs Piggie")
+
+else:
+
+    print("Alien animal")
+    
+
---
+++ testloop.py
@@ -0,0 +1,15 @@
+
+'''
+
+for a in range(10,17):
+
+    print (a)
+
+'''
+
+a = 2000
+while a < 2060:
+	print (a)
+	a+=4
+
+
