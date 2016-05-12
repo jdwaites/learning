fa7cc527a10830368afae4c80207aff47a0b7efb
jdwaites <jamal_waites@yahoo.com>
2016-05-12 10:17:25 -0400

    adding testcsv

--- csvsamples.py
+++ csvsamples.py
@@ -1,5 +1,14 @@
+"""
 import csv
 with open('test.csv','rb') as f:
     reader = csv.reader(f)
+    for row in reader:
+        print (row)
+
+"""
+
+import csv
+with open('test.csv','rb') as f:
+    reader = csv.reader(f, delimiter=':', quoting=csv.QUOTE_NONE)
     for row in reader:
         print (row)
